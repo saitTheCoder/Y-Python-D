4f41b9ae588fe9ee5fcd8d7d4427c2f39318d6f9
Skytech <93709385+saitTheCoder@users.noreply.github.com>
2023-07-22 21:00:57 +0300

    Add files via upload

---
+++ Y Python D/first.py
@@ -0,0 +1,40 @@
+print("hello")
+# print(10+15)
+
+# Basic Data Types - Temel Veri Tipleri
+# 1. String - "ahmet" 'istanbul' "100$@!"
+# 2. Numbers - 100 , 100.99
+# Integer - Tam sayilar - 100
+# 100 - "100"
+# Floating Numbers - Ondalikli sayilar - 100.99
+
+# Variables - Degiskenler
+# speed_limit = 120
+# adult_age = 18
+# xyz = 10
+# def = 10
+# print(help("keywords"))
+
+# print(speed_limit)
+# name = "Salih"
+# print(name)
+
+kardes1 = "Rafet" 
+kardes2 = "Ayse" 
+
+print(kardes1+" ve "+kardes2+" bize geldi.")
+print("10"+"8") # "108"
+
+user = "ahmet06"
+nickname = "panda"
+
+print("Kullanici adi "+user+" olan " +"nickname i "+nickname+ " olan sahis sisteme giris yapti!!!!!")
+
+# Input Fonksiyonu - Kullanicidan Girdi almayi saglar
+print(1)
+name = input("Adiniz nedir? ") # name = "sait"
+# name = "mehmet" # name = "sait"
+print(2)
+# name = input(" ")
+# print(name*3)
+# print(name)
